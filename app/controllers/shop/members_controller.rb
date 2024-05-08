6f6741945a0cd6c1f07805e02cf157f05adfec6a
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-05-08 12:20:27 +0000

    ショップ側でユーザーごとの予約履歴一覧作成

--- app/controllers/shop/members_controller.rb
+++ app/controllers/shop/members_controller.rb
@@ -1,5 +1,10 @@
 class Shop::MembersController < ApplicationController
   before_action :authenticate_shop!
+
   def show
+    @shop = current_shop
+    @shop_items = Item.where(shop_id: @shop.id)
+    @member = Member.find(params[:id])
+    @pre_orders = PreOrder.where(item_id: @shop_items.pluck(:id), member_id: @member)
   end
 end
