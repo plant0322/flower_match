48316aad56b4ad1073b03f966417543e251ccc22
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-05-08 14:08:27 +0000

    管理者側のユーザー関連ページ作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,11 +1,26 @@
 class Admin::MembersController < ApplicationController
   before_action :authenticate_admin!
   def show
+    @member = Member.find(params[:id])
   end
 
   def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.update(member_params)
+    redirect_to request.referer
   end
 
   def index
+    @members = Member.all
   end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :postal_code, :address, :telephone_number, :is_active)
+  end
+
 end
