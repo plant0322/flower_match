18cf3e7d725b5cee0156a049cced197c4fe06121
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-05-17 08:55:33 +0000

    [Add]itemのタグ登録機能追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,7 @@
 class Tag < ApplicationRecord
 
-  has_many :item_tags, dependent: :destroy
+  has_many :item_tags, dependent: :destroy, foreign_key: 'tag_id'
+  has_many :items, through: :item_tags
 
   validates :name, presence: true
 end
