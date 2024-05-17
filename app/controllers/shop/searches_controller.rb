e9f01839045fc60b160c7c842a337b58ccfd0e37
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-05-17 06:28:47 +0000

    [Fix]shop側予約の検索で他ショップのデータも表示してしまう不具合修正

--- app/controllers/shop/searches_controller.rb
+++ app/controllers/shop/searches_controller.rb
@@ -1,24 +1,37 @@
 class Shop::SearchesController < ApplicationController
+  before_action :authenticate_shop!
+
   def search
     @type = params[:type]
     @method = params[:method]
     @content = params[:content]
+    shop_items = Item.where(shop_id: current_shop.id)
 
     if @type == 'order_member'
-      @records = PreOrder.where(['last_name LIKE? OR first_name LIKE? OR last_name_kana LIKE? OR first_name_kana LIKE? OR
+      content_records = PreOrder.where(['last_name LIKE? OR first_name LIKE? OR last_name_kana LIKE? OR first_name_kana LIKE? OR
                                   telephone_number LIKE? OR postal_code LIKE? OR address LIKE?',
                                   '%'+@content+'%', '%'+@content+'%', '%'+@content+'%', '%'+@content+'%', '%'+@content+'%', '%'+@content+'%', '%'+@content+'%'])
+      @records = PreOrder.where(item_id: shop_items.pluck(:id)).and(content_records)
     elsif @type == 'order_item_all'
       order_records = PreOrder.where(['name LIKE?','%'+@content+'%'])
+                              .where(item_id: shop_items.pluck(:id))
       item_ids = Item.where(['name LIKE? OR introduction LIKE?','%'+@content+'%','%'+@content+'%'])
       item_records = PreOrder.where(item_id: item_ids)
-      @records = order_records + item_records
+                             .where(item_id: shop_items.pluck(:id))
+      @records = (order_records + item_records).uniq
     elsif @type == 'order_item'
       order_records = PreOrder.search_for(@content, @method)
+                              .where(item_id: shop_items.pluck(:id))
       item_ids = Item.search_for(@content, @method)
       item_records = PreOrder.where(item_id: item_ids)
-      @records = order_records + item_records
-    else
+                             .where(item_id: shop_items.pluck(:id))
+      @records = (order_records + item_records).uniq
+    elsif @type == 'item'
+      @records = Item.where(['name LIKE? OR introduction LIKE?','%'+@content+'%','%'+@content+'%'])
+                     .where(id: shop_items.pluck(:id))
+    else # @type == 'item_name'
+      @records = Item.search_for(@content, @method)
+                     .where(id: shop_items.pluck(:id))
     end
   end
 end
