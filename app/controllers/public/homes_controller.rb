2c8ebbe0cdeb35016f8a41531d47e89148e492b0
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-05-23 20:03:47 +0000

    [Fix]非公開商品と退会したショップの商品表示設定

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
-    @active_items = Item.where(is_active: true)
-    @items = @active_items.order(id: "DESC").page(params[:page])
+    active_shops = Shop.where(is_active: true)
+    active_items = Item.where(is_active: true)
+    items_all = Item.where(id: active_items, shop_id: active_shops)
+    @items = items_all.order(updated_at: "DESC").page(params[:page])
     @tag_rank = Tag.tag_rank_item
   end
 
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,17 +1,21 @@
 class Public::SearchesController < ApplicationController
 
   def search
+    active_shops = Shop.where(is_active: true)
     @content = params[:content]
     @model = params[:model]
     @tag_rank = Tag.tag_rank_item
     @tags = Tag.tag_rank_item.limit(50)
     if @model == 'item'
       @records = Item.where('name LIKE? OR introduction LIKE?','%'+@content+'%','%'+@content+'%').order(id: 'DESC')
-                     .where(is_active: true).page(params[:page])
+                     .where(is_active: true, shop_id: active_shops).page(params[:page])
     elsif @model == 'shop'
-      @records = Shop.where('name LIKE? OR name_kana LIKE?', '%'+@content+'%', '%'+@content+'%').order(id: 'DESC').page(params[:page])
+      @records = Shop.where('name LIKE? OR name_kana LIKE?', '%'+@content+'%', '%'+@content+'%').order(id: 'DESC')
+                     .where(is_active: true).page(params[:page])
     else #@model = 'tag'
-      @records = Tag.search_items(@content)
+      active_shops = Shop.where(is_active: true)
+      active_items = Item.where(is_active: true, shop_id: active_shops)
+      @records = Tag.search_items(@content, active_items)
     end
   end
 end
