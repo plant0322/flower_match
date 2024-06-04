c7d01ec6679d2fe309cb989fdd3bd5108f5aab6a
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-06-04 10:44:14 +0000

    [Add]管理者側の商品一覧作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
     resources :shops, only: [:index, :edit, :update]
     resources :members, only: [:show, :index, :edit, :update]
+    resources :items, only: [:index]
     resources :reviews, only: [:index, :update]
     resources :tags, only: [:index, :create, :update, :destroy]
     resources :pick_up_tags, only: [:edit, :create, :update, :destroy]
