04707d9adff1c1e583311e4caa607483f1200163
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-07-22 15:03:53 +0000

    [Add]郵便番号から住所の自動入力機能追加

--- app/javascript/packs/hamburger_menu.js
+++ app/javascript/packs/hamburger_menu.js
@@ -1,6 +1,5 @@
-document.addEventListener('turbolinks:load', function () {
+$(function () {
   $('#js-hamburger-menu').on('click', function() {
     $('.navigation').slideToggle(500)
-    // ('.hamburger-menu').toggleClass('hamburger-menu-open')
   });
 });
---
+++ app/javascript/packs/postcode_autofill.js
@@ -0,0 +1,11 @@
+jQuery(document).on('turbolinks:load', function () {
+  $('#shop_postal_code').jpostal({
+    postcode : [
+      '#shop_postal_code'
+    ],
+    address : {
+      '#shop_prefecture_code' : '%3',
+      '#shop_address'    : '%4%5',
+    }
+  });
+});
